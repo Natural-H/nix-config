c94037c0b4e163eb315d3f9ff1cce262796d9fad
naturalh <marco.mmtz@proton.me>
2025-09-10 13:20:45 -0600

    remove gamescope from thinkbook16

--- machines/nix-thinkbook16.nix
+++ machines/nix-thinkbook16.nix
@@ -60,17 +60,12 @@
   programs.zsh.enable = true;
 
   programs.gamemode.enable = true;
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;
-  };
 
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    gamescopeSession.enable = true;
   };
 
   services = {
