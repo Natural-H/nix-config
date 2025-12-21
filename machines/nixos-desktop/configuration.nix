80f90ae212d584599be3ae281f198e5618eeaddf
naturalh <marco.mmtz@proton.me>
2025-12-21 10:59:04 -0600

    remove fallback dns in favor to the default list

--- machines/nixos-desktop/configuration.nix
+++ machines/nixos-desktop/configuration.nix
@@ -135,10 +135,6 @@
       dnssec = "true";
       dnsovertls = "opportunistic";
       domains = ["~."];
-      fallbackDns = [
-        "8.8.8.8"
-        "2001:4860:4860::8844"
-      ];
     };
 
     tailscale = {
