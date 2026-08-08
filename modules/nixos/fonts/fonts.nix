41f354a8b1e7fcced4ab88b189404a3a3b46c84d
naturalh <marco.mmtz@proton.me>
2026-08-08 13:17:11 -0600

    enable fontDir

--- modules/nixos/fonts/fonts.nix
+++ modules/nixos/fonts/fonts.nix
@@ -13,26 +13,30 @@
   };
 
   config = lib.mkIf config.fonts.includeCommon {
-    fonts.packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-color-emoji
-      liberation_ttf
-      fira-code
-      fira-code-symbols
-      mplus-outline-fonts.githubRelease
-      dina-font
-      proggyfonts
-      nerd-fonts.fira-code
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.droid-sans-mono
-      corefonts
-      vista-fonts
-      oswald
-      lexend
-      roboto
-      montserrat
-      liberation-sans-narrow
-    ];
+    fonts = {
+      fontDir.enable = true;
+
+      packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-color-emoji
+        liberation_ttf
+        fira-code
+        fira-code-symbols
+        mplus-outline-fonts.githubRelease
+        dina-font
+        proggyfonts
+        nerd-fonts.fira-code
+        nerd-fonts.jetbrains-mono
+        nerd-fonts.droid-sans-mono
+        corefonts
+        vista-fonts
+        oswald
+        lexend
+        roboto
+        montserrat
+        liberation-sans-narrow
+      ];
+    };
   };
 }
