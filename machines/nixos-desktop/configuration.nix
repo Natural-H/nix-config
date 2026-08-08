d6462fe62b07991a0cc8dc2983ca0a14ec508b78
naturalh <marco.mmtz@proton.me>
2026-08-07 20:45:21 -0600

    enable secure boot for desktop

--- machines/nixos-desktop/configuration.nix
+++ machines/nixos-desktop/configuration.nix
@@ -20,6 +20,11 @@
       package = pkgs.limine-full;
       efiSupport = true;
       maxGenerations = 50;
+      secureBoot = {
+        enable = true;
+        autoEnrollKeys.enable = true;
+        autoGenerateKeys = true;
+      };
       extraEntries = ''
         /Windows
           protocol: efi
@@ -85,6 +90,7 @@
     unrar
     geoclue2
     net-tools
+    sbctl
 
     wayland-utils
     wl-clipboard
