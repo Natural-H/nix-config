beb30b7b44193d95b6b7762faabeddab283afc58
naturalh <marco.mmtz@proton.me>
2026-09-16 15:15:10 -0600

    Change from nixpkgs to nixos ref (That explains a lot)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flatpaks.url = "github:gmodena/nix-flatpak/?ref=latest";
 
     hyprland.url = "github:hyprwm/Hyprland";
